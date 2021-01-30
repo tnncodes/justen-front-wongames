ec96289382c722dff22d12a16574f57016fc3de0
theandersonn <theandersonn@gmail.com>
2021-01-29 22:54:43 -0300

    Logo: testando as cores

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,25 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
+  // Passos do teste
+  // 1. renderizar o componente `render`
+  // 2. selecionar o elemento a ser testado `screen` (queries) - getByLabel...
+  // 3. expect - assertion - asserção - comparação - análise - (espero que renderize a logo branca)
+
   it('should render a white label by default', () => {
-    // 1. renderizar o componente `render`
-    // 2. selecionar o elemento a ser testado `screen` (queries) - getByLabel...
-    // 3. expect - assertion - asserção - comparação - análise - (espero que renderize a logo branca)
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#fafafa'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
