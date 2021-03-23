99521955915560f9785a833d0b4daa5f4abda63f
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-03-23 10:13:12 -0300

    GameCard parte 5: testes

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
@@ -38,5 +39,33 @@ describe('<GameCard />', () => {
     // verificar se o price foi renderizado
     // ------------------------------------
     expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    // renderizar o componente
+    renderWithTheme(<GameCard {...props} />)
+
+    const price = screen.getByText('R$ 235,00')
+
+    // preço não tenha line-through
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+
+    // preço não tenha o background secundário
+    expect(price).toHaveStyle({ backgroundColor: theme.colors.secondary })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    // renderiza o componente com promotionalPrice // 200 // 15 reais
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />)
+
+    // preço tenha line-through (235)
+    expect(screen.getByText('R$ 235,00')).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+
+    // preço novo promocional não vai ter line-through (15)
+    expect(screen.getByText('R$ 15,00')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
   })
 })
