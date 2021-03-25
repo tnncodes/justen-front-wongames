f35d3b6753128ecfdcc8c952415cdf8fe82bc0a5
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-03-25 11:01:37 -0300

    Slider Parte 3: Criando testes e o mock para o matchMedia

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -1,13 +1,31 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
 
-describe('Slider', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Slider />)
+describe('<Slider />', () => {
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 2, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    // verifica se o item 1 contem a classe slick-slide
+    // ------------------------------------------------
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
 
+    // verifica se o item 2 contem a classe slick-slide
+    // ------------------------------------------------
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    // gera o snapshot
+    // ---------------
     expect(container.firstChild).toMatchSnapshot()
   })
 })
