d64d4d1a533cc10549a40ec24ed6a99184dff347
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-03-30 21:05:44 -0300

    Criando estrutura inicial do template

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,9 +1,23 @@
-import { render } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
-describe('Auth', () => {
-  it('<Auth />', () => {
-    render(<Auth />)
+describe('<Auth />', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
+
+    // verificar se tem 2 loogos
+
+    // verificar se tem o heading principal do banner
+
+    // verificar se tem o subtitle
+
+    // verificar se tem o title do content
+
+    // verificar se o children está sendo renderizado
   })
 })
