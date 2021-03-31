06cb74f917a6567fb2a25fb15b074f616c84e56e
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-03-31 20:35:17 -0300

    Criando o componente de FormSignIn

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { Email, Lock } from '@styled-icons/material-outlined'
+
+import TextField from 'components/TextField'
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+const FormSignIn = () => (
+  <S.Wrapper>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgetPassword href="#">Forget your password</S.ForgetPassword>
+
+      <Button size="large" fullWidth>
+        Sign in now
+      </Button>
+
+      <S.FormLink>
+        Dont`t have an account?{' '}
+        <Link href="/">
+          <a>Sign up</a>
+        </Link>
+      </S.FormLink>
+    </form>
+  </S.Wrapper>
+)
+
+export default FormSignIn
---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,24 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    // verifique email
+
+    // verifique password
+
+    // verifique button
+  })
+
+  it('should render the forgot password link', () => {
+    // test
+  })
+
+  it('should render the text and link to sign up', () => {
+    // text
+    // link
+  })
+})
