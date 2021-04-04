592acf78cdc8ac17ba35753e4ce0a7a72dddc986
theandersonn <theandersonn@gmail.com>
2021-04-04 16:15:29 -0300

    GameDetails: Formatando datetime com Intl e testes

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -8,13 +8,19 @@ import * as S from './styles'
 type Platform = 'windows' | 'linux' | 'mac'
 
 export type GameDetailsProps = {
+  developer: string
   platforms: Platform[]
+  releaseDate: string
 }
 
-const GameDetails = ({ platforms }: GameDetailsProps) => {
+const GameDetails = ({
+  developer,
+  releaseDate,
+  platforms
+}: GameDetailsProps) => {
   const platformIcons = {
     linux: <Linux title="Linux" size={18} />,
-    mac: <Apple title="Apple" size={18} />,
+    mac: <Apple title="Mac" size={18} />,
     windows: <Windows title="Windows" size={18} />
   }
 
@@ -29,12 +35,18 @@ const GameDetails = ({ platforms }: GameDetailsProps) => {
       <S.Content>
         <S.Block>
           <S.Label>Developer</S.Label>
-          <S.Description>Gearbox Software</S.Description>
+          <S.Description>{developer}</S.Description>
         </S.Block>
 
         <S.Block>
           <S.Label>Release Date</S.Label>
-          <S.Description>Nov 16, 2019</S.Description>
+          <S.Description>
+            {new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(releaseDate))}
+          </S.Description>
         </S.Block>
 
         <S.Block>
@@ -47,8 +59,13 @@ const GameDetails = ({ platforms }: GameDetailsProps) => {
         </S.Block>
 
         <S.Block>
-          <S.Label>Rating</S.Label>
+          <S.Label>Publisher</S.Label>
           <S.Description>18+</S.Description>
+        </S.Block>
+
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>2K</S.Description>
         </S.Block>
 
         <S.Block>
--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,12 +1,50 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
-describe('GameDetails', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameDetails platforms={['windows', 'linux', 'mac']} />)
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'mac', 'linux'],
+  releaseDate: '2020-11-21T23:00:00'
+}
 
-    expect(screen.getByText(/Game Details/i)).toBeInTheDocument()
+describe('<GameDetails />', () => {
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+  })
+
+  it('should render platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+  })
+
+  it('should render the formated date', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
   })
 })
