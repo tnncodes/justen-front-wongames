b4b6120a2f373376e14a26bfa4c15724b768c091
theandersonn <theandersonn@gmail.com>
2021-04-04 14:44:19 -0300

    GameDetails: estrutura inicial

---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameDetails from '.'
+
+describe('GameDetails', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameDetails />)
+
+    expect(screen.getByText(/Game Details/i)).toBeInTheDocument()
+  })
+})
