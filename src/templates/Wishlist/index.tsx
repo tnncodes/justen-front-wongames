f29a447bb0388fe478dcecc5428eb631ac8ddd2e
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-04-15 18:36:06 -0300

    Wishlist: Showcase, dados e testes

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,14 +1,32 @@
-import { Container } from 'components/Container'
-import Heading from 'components/Heading'
 import Base from 'templates/Base'
 
-const Wishlist = () => (
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
