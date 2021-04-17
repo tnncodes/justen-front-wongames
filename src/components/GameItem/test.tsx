7a2f8c54a62c6e4109d18ff41e6f5df71eac08a6
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-04-17 11:17:50 -0300

    GameItem: Link de download

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -27,4 +27,14 @@ describe('<GameItem />', () => {
     // verificar se o preço foi renderizado
     expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
   })
+
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
 })
