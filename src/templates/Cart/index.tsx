e6ad8b1b0580b3034818c6a1e5dab8b92950885d
theandersonn <theandersonn@gmail.com>
2021-04-20 21:18:27 -0300

    CartPage: Estrutura da pagina

---
+++ src/templates/Cart/index.tsx
@@ -0,0 +1,54 @@
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+import CartList, { CartListProps } from 'components/CartList'
+
+import { Divider } from 'components/Divider'
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import Base from 'templates/Base'
+
+import * as S from './styles'
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) => {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My cart
+        </Heading>
+
+        <S.Content>
+          <CartList items={items} total={total} />
+
+          <PaymentOptions cards={cards} handlePayment={handlePayment} />
+        </S.Content>
+
+        <Divider />
+      </Container>
+
+      <Showcase
+        title="You may like games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
+
+export default Cart
