744301770a37713fb24a2bb387970f1e06737b57
theandersonn <theandersonn@gmail.com>
2021-05-03 21:25:39 -0300

    Limpa testes feitos com Apollo

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -10,7 +10,6 @@ import * as S from './styles'
 import Base from 'templates/Base'
 
 export type HomeTemplateProps = {
-  data: any
   banners: BannerProps[]
   newGames: GameCardProps[]
   mostPopularHighlight: HighlightProps
