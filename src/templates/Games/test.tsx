e48146edc6075cc2542b412751fa889017265ab6
theandersonn <theandersonn@gmail.com>
2021-05-26 22:50:48 -0300

    add mock next/link para auxiliar testes

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -27,25 +27,32 @@ jest.mock('templates/Base', () => ({
   }
 }))
 
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div>{children}</div>
+  }
+}))
+
 describe('<Games />', () => {
-  // it('should render sections', async () => {
-  //   renderWithTheme(
-  //     <MockedProvider mocks={[gamesMock]} addTypename={false}>
-  //       <Games filterItems={filterItemsMock} />
-  //     </MockedProvider>
-  //   )
+  it('should render sections', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
 
-  //   // we wait until we have data to get the elements
-  //   // get => tem certeza do elemento
-  //   // query => Não tem o elemento
-  //   // find => processos assincronos
-  //   expect(await screen.findByText(/Price/i)).toBeInTheDocument()
-  //   expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
+    // we wait until we have data to get the elements
+    // get => tem certeza do elemento
+    // query => Não tem o elemento
+    // find => processos assincronos
+    expect(await screen.findByText(/Price/i)).toBeInTheDocument()
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
-  //   expect(
-  //     await screen.findByRole('button', { name: /show more/i })
-  //   ).toBeInTheDocument()
-  // })
+    expect(
+      await screen.findByRole('button', { name: /show more/i })
+    ).toBeInTheDocument()
+  })
 
   it('should render empty when no games found', async () => {
     renderWithTheme(
@@ -59,34 +66,34 @@ describe('<Games />', () => {
     ).toBeInTheDocument()
   })
 
-  // it('should render more games when show more is clicked', async () => {
-  //   renderWithTheme(
-  //     <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
-  //       <Games filterItems={filterItemsMock} />
-  //     </MockedProvider>
-  //   )
+  it('should render more games when show more is clicked', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
 
-  //   expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
-  //   userEvent.click(await screen.findByRole('button', { name: /show more/i }))
+    userEvent.click(await screen.findByRole('button', { name: /show more/i }))
 
-  //   expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
-  // })
+    expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
+  })
 
-  // it('should change push router when selecting a filter', async () => {
-  //   renderWithTheme(
-  //     <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
-  //       <Games filterItems={filterItemsMock} />
-  //     </MockedProvider>
-  //   )
+  it('should change push router when selecting a filter', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} cache={apolloCache}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
 
-  //   userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
-  //   userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
-  //   userEvent.click(await screen.findByLabelText(/low to high/i))
+    userEvent.click(await screen.findByRole('checkbox', { name: /windows/i }))
+    userEvent.click(await screen.findByRole('checkbox', { name: /linux/i }))
+    userEvent.click(await screen.findByLabelText(/low to high/i))
 
-  //   expect(push).toHaveBeenCalledWith({
-  //     pathname: '/games',
-  //     query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
-  //   })
-  // })
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/games',
+      query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
+    })
+  })
 })
