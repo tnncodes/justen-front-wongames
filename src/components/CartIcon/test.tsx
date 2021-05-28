00b149a6ccfd2ea3617b1d9733be8cc6af875ad2
theandersonn <theandersonn@gmail.com>
2021-05-28 13:21:43 -0300

    Editando o CartIcon para usar o UseCart hook

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -1,3 +1,4 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import CartIcon from '.'
@@ -19,25 +20,14 @@ describe('<CartIcon />', () => {
   // Teste sem badge
   // ---------------
   it('should render with badge', () => {
-    render(<CartIcon quantity={3} />)
+    render(<CartIcon />, {
+      cartProviderProps: { ...CartContextDefaultValues, quantity: 3 }
+    })
 
     // verificar se a badge esteja no documento
     expect(screen.getByLabelText(/cart items/i)).toBeInTheDocument()
 
     // verificar a quantidade de itens no badge
     expect(screen.getByText(/3/)).toBeInTheDocument()
-  })
-
-  // Testar para aceitar somente numeros positivos
-  // ---------------------------------------------
-  it('should render with badge only if has positive numbers', () => {
-    render(<CartIcon quantity={-1} />)
-
-    // não espero encontrar o badge
-    // em consultas que não espera encontrar determinado elemento, utilizar query
-    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
-
-    // não espero numeros negativos
-    expect(screen.queryByText(/-1/)).not.toBeInTheDocument()
   })
 })
