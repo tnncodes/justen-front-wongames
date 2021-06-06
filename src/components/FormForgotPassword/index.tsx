ca00786a6b19871e27bfd60fb819f342ee6f6950
theandersonn <theandersonn@gmail.com>
2021-06-06 10:27:18 -0300

    Editando a pagina de /profile/me no reset de senha

--- src/components/FormForgotPassword/index.tsx
+++ src/components/FormForgotPassword/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import { FieldErrors, forgotValidate } from 'utils/validations'
 import {
   CheckCircleOutline,
@@ -16,10 +17,11 @@ import TextField from 'components/TextField'
 import Button from 'components/Button'
 
 const FormForgotPassword = () => {
+  const { query } = useRouter()
   const [success, setSuccess] = useState(false)
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '' })
+  const [values, setValues] = useState({ email: (query.email as string) || '' })
   const [loading, setLoading] = useState(false)
 
   const handleInput = (field: string, value: string) => {
@@ -83,6 +85,7 @@ const FormForgotPassword = () => {
               placeholder="Email"
               type="text"
               error={fieldError?.email}
+              initialValue={query.email as string}
               onInputChange={(v) => handleInput('email', v)}
               icon={<Email />}
             />
