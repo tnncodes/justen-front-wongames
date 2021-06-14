ff1f97ebb4ee3d7925b4fe9c9628be587de9c1b6
theandersonn <theandersonn@gmail.com>
2021-06-14 19:27:46 -0300

    Desabilitando botao de compra quando tiver erro ou campos vazios

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -9,8 +9,10 @@ import * as S from './styles'
 
 const PaymentForm = () => {
   const [error, setError] = useState<string | null>(null)
+  const [disabled, setDisabled] = useState(true)
 
   const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setDisabled(event.empty)
     setError(event.error ? event.error.message : '')
   }
 
@@ -46,7 +48,11 @@ const PaymentForm = () => {
           Continue shopping
         </Button>
 
-        <Button fullWidth icon={<ShoppingCart />}>
+        <Button
+          fullWidth
+          icon={<ShoppingCart />}
+          disabled={disabled || !!error}
+        >
           Buy now
         </Button>
       </S.Footer>
