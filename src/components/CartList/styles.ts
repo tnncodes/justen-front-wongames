ff144b3f72258225e540649a8256d25d55dc2023
theandersonn <theandersonn@gmail.com>
2021-06-19 16:14:41 -0300

    Criando metodo de getImageUrl

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -13,22 +13,15 @@ export const Wrapper = styled.div<WrapperProps>`
     background: ${theme.colors.white};
     display: flex;
     flex-direction: column;
-    align-self: flex-start;
-
+    align-self: start;
     ${isEmpty &&
     css`
       ${EmptyStyles.Wrapper} {
         padding-bottom: ${theme.spacings.medium};
       }
-
-      ${EmptyStyles.Image} {
-        max-width: 20rem;
-      }
-
       ${EmptyStyles.Title} {
         font-size: ${theme.font.sizes.large};
       }
-
       ${EmptyStyles.Description} {
         color: ${theme.colors.black};
         font-size: ${theme.font.sizes.medium};
@@ -67,7 +60,6 @@ export const Footer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.medium};
       padding: ${theme.spacings.small};
