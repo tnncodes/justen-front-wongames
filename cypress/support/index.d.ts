09790f75002b983c307fbfa2c5eb89d65ea1e8c6
theandersonn <theandersonn@gmail.com>
2021-06-22 08:22:46 -0300

    Utilizando assertions e criando comando para showcase

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,10 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
 declare namespace Cypress {
   interface Chainable {
     /**
@@ -14,5 +18,11 @@ declare namespace Cypress {
      * @example cy.shouldRenderBanner()
      */
     shouldRenderBanner(): Chainable<Element>
+
+    /**
+     * Custom command to check Showcase in page
+     * @example cy.shouldRenderShowcase()
+     */
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
