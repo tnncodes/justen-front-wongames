00b093fb3eee9423c9fe2260c38d5794a2fabf3d
theandersonn <theandersonn@gmail.com>
2021-06-25 09:20:57 -0300

    comando customizado para o SignIn

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -15,13 +15,7 @@ describe('User', () => {
 
   it('should sign in and sign out', () => {
     cy.visit('/sign-in')
-
-    cy.findAllByPlaceholderText(/email/i).type('e2e@wongames.com')
-    cy.findAllByPlaceholderText(/password/i).type('123456')
-    cy.findByRole('button', { name: /sign in now/i }).click()
-
-    cy.findByText(/cypress/i).should('exist').click()
-    cy.findByText(/sign out/i).click()
+    cy.signIn()
 
     cy.findByRole('link', { name: /sign in/i }).should('exist')
     cy.findByText(/cypress/i).should('not.exist')
