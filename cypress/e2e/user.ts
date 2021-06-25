f513dc3f0a40ae1fa3a10245e6684eb4f72f5043
theandersonn <theandersonn@gmail.com>
2021-06-25 09:14:09 -0300

    Fazendo o teste para o SignIn

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,10 +3,27 @@
 import { createUser } from '../support/generate'
 
 describe('User', () => {
-  it('should sign up', () => {
+  it.skip('should sign up', () => {
     const user = createUser()
 
     cy.visit('/sign-up')
     cy.signUp(user)
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.findByText(user.username).should('exist')
+  });
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findAllByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findAllByPlaceholderText(/password/i).type('123456')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
   });
 });
